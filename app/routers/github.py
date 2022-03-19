7e2aa65e60d7920a3e61c3bafb139570a6640a5c
huscker <egostalker03@mail.ru>
2022-03-19 04:32:28 +0300

    added repo getter

--- app/routers/github.py
+++ app/routers/github.py
@@ -11,7 +11,7 @@ async def get_github_info(github_name: str, github_token: Optional[str] = None):
     if not github_token:
         pass
     url = f'https://api.github.com/users/{github_name}'
-    result = requests.get(url,headers={'Authorization': 'token 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'})
+    result = requests.get(url)
     if result.status_code != 200:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
     result = result.json()
@@ -20,15 +20,31 @@ async def get_github_info(github_name: str, github_token: Optional[str] = None):
         'name' : github_name,
     }
     try:
-        user['total_repos'] = 0
-        user['total_repos'] += result['public_repos'] 
-        user['total_repos'] += result['total_private_repos']
+        user['public_repos'] = result['public_repos'] 
     except Exception as er:
         print(er)
     try:
         user['pro'] = result['plan']['name'] == 'pro'
     except Exception as er:
         print(er)
+        
+    url = f'https://api.github.com/users/{github_name}/repos'
+    result = requests.get(url)
+    if result.status_code != 200:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+    result = result.json()
+    
+    repos = list()
+    try:
+        for i in result:
+            repos.append({
+                'name':i['name'],
+                'language': i['language'],
+                'created_at': i['created_at'],
+            })
+    except Exception as er:
+        print(er)
+    user['repos'] = repos
     return JSONResponse(status_code=status.HTTP_200_OK,content={
         'details':user,
     })
