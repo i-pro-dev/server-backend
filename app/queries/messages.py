d963be89092071ba0d3ec2acbb0a0a4735330a30
huscker <egostalker03@mail.ru>
2022-03-20 04:11:22 +0300

    testing new time of sending

--- app/queries/messages.py
+++ app/queries/messages.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from fastapi import HTTPException,status
 
 async def send_message(from_user_id: int, to_user_id: int, message: str):
-    if not await DB.execute('insert into messages(from_user_id,to_user_id,message,updated) values ($1,$2,$3,$4)',from_user_id,to_user_id,message,datetime.now()):
+    if not await DB.execute('insert into messages(from_user_id,to_user_id,message,sended) values ($1,$2,$3,$4)',from_user_id,to_user_id,message,datetime.now()):
        HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
     
 async def get_messages(from_user_id:int, to_user_id:int):
